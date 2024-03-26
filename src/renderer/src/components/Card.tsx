35bd451e6106d92cda8128c52e5bf7815817332b
Joseph Silva <119358195+JosephSC0121@users.noreply.github.com>
2024-03-25 21:53:50 -0500

    Update card component

--- src/renderer/src/components/Card.tsx
+++ src/renderer/src/components/Card.tsx
@@ -1,50 +1,47 @@
-import PropTypes from 'prop-types' // Importa PropTypes
-import Box from '@mui/joy/Box'
-import Button from '@mui/joy/Button'
-import Card from '@mui/joy/Card'
-import CardContent from '@mui/joy/CardContent'
-import CardActions from '@mui/joy/CardActions'
-import IconButton from '@mui/joy/IconButton'
-import Typography from '@mui/joy/Typography'
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder'
+import PropTypes from 'prop-types'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import { CardActionArea } from '@mui/material'
 
 export default function BottomActionsCard() {
   return (
     <div className="bg-primary flex justify-center items-center h-screen">
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        <CustomCard />
-        <CustomCard />
-        <CustomCard title="Custom Title" content="Custom Content" buttonText="Custom Button Text" />
-        <CustomCard />
+        <ActionAreaCard />
+        <ActionAreaCard />
+        <ActionAreaCard
+          title="Custom Title"
+          content="Custom Content"
+          image="https://i.pinimg.com/564x/91/5b/c8/915bc855b3b1dc43f7d1bcd784386863.jpg"
+        />
+        <ActionAreaCard />
       </div>
     </div>
   )
 }
 
-function CustomCard({ title, content, buttonText }) {
+function ActionAreaCard({ title, content, image }) {
   return (
-    <Card variant="outlined" className="w-full sm:w-80 overflow-auto resize-x">
-      <Box className="flex justify-between items-center"></Box>
-      <CardContent>
-        <Typography level="title-lg">{title}</Typography>
-        <Typography level="body-sm">{content}</Typography>
-      </CardContent>
-      <CardActions className="flex justify-between items-center">
-        <IconButton variant="outlined" color="neutral" className="mr-auto">
-          <FavoriteBorder />
-        </IconButton>
-        <Button variant="outlined" color="neutral">
-          View
-        </Button>
-        <Button variant="solid" color="primary">
-          {buttonText}
-        </Button>
-      </CardActions>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia component="img" height="140" image={image} alt="green iguana" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {content}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
     </Card>
   )
 }
-CustomCard.propTypes = {
+
+ActionAreaCard.propTypes = {
   title: PropTypes.string,
   content: PropTypes.string,
-  buttonText: PropTypes.string
+  image: PropTypes.string
 }
--- src/renderer/src/pages/MainPage.tsx
+++ src/renderer/src/pages/MainPage.tsx
@@ -3,7 +3,6 @@ import BottomActionsCard from '@renderer/components/Card'
 function MainPage(): JSX.Element {
   return (
     <div className="bg-primary flex flex-col h-screen">
-      
       <SideBar />
       <BottomActionsCard />
     </div>
